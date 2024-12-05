c20f70a74f1a9cb442044bbf23816afdc99b7f79
Mohammad Salim <mhmadip@gmail.com>
2024-12-05 12:08:20 +0300

    completing the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,63 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
-  return runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: const Text('Dicee'),
-          backgroundColor: Colors.red,
-        ),
-        body: DicePage(),
-      ),
-    ),
-  );
+  runApp(const DiceApp());
 }
 
-class DicePage extends StatelessWidget {
+class DiceApp extends StatelessWidget {
+  const DiceApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home:  Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: const Text('Dice App TIU'),
+          backgroundColor: Colors.redAccent,
+        ),
+        body: const DicePage(),
+      ),
+    );
+  }
+}
+
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  final Random _random = Random();
+  int dice1 = 1;
+  int dice2 = 1;
+
+
+  void rollDice() {
+    setState(() {
+      dice1 = _random.nextInt(6) + 1;
+      dice2 = _random.nextInt(6) + 1;
+
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: rollDice,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center, // Centers the row content
+          children: [
+            Expanded(child: Image.asset('assets/dice$dice1.png')), // First die
+            const SizedBox(width: 20), // Space between the dice
+            Expanded(child: Image.asset('assets/dice$dice2.png')), // Second die
+          ],
+        ),
+      ),
+    );
   }
 }
